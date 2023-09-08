af2270d00d561c7d2e9ce3562ad2f86c5c648cf0
Pipeline <catalinaaristizabal312613@correo.itm.edu.co>
2023-09-07 22:37:39 -0500

    1er Ejercicio

--- Helloworld/Program.cs
+++ Helloworld/Program.cs
@@ -1,3 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadLine();
+﻿namespace Ejercicios{
+    class Program{
+        static void Main(string[] args){
+            
+            /*Positive Power: 
+            Solicita al usuario un número.
+            Eleva este número al cuadrado solo si es positivo. */ 
+         
+         Console.WriteLine("POSITIVE POWER");       
+           Console.WriteLine("Digite un número");
+           if (int.TryParse(Console.ReadLine(), out int numero)) 
+           {
+           
+               if (numero > 0)
+               {
+               int resultado = numero * numero;
+               Console.WriteLine ("El número " + numero + " elevado al cuadrado es " + resultado);
+               }
+
+               else if (numero < 0)
+               {
+               Console.WriteLine (" El número es negativo");
+               } 
+              
+               else 
+               {
+               Console.WriteLine(" Entrada no válida. Ingresa un número válido.");
+               }
+            } 
+
+            else { Console.WriteLine(" Entrada no válida. Ingresa un número válido.");}
+
+
+
+
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+    } 
+}
